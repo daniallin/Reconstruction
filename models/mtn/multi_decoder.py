469185cf86fcd51e45529b406158bff84ea884ff
lzy <lzhy9503@163.com>
2019-09-02 22:33:11 +0800

    fix

--- models/mtn/multi_decoder.py
+++ models/mtn/multi_decoder.py
@@ -16,56 +16,51 @@ def _conv_layer(in_chan, out_chan, k, s=1, p=1, sync_bn=False):
 
 
 class Attention(nn.Module):
-    def __init__(self, pedestrian_num, hidden_size, method, use_cuda, bilstm):
+    def __init__(self, in_chans, out_chans, method):
         super(Attention, self).__init__()
-        self.pedestrian_num = pedestrian_num
-        self.hidden_size = hidden_size
         self.method = method
-        self.use_cuda = use_cuda
         # Define layers
         if self.method == 'general':
-            self.attention = nn.Linear(self.hidden_size * bilstm, self.hidden_size)
+            self.attention = nn.Conv2d(in_chans, out_chans, 3, padding=1)
         elif self.method == 'concat':
-            self.attention = nn.ModuleList([
-                nn.Linear(self.hidden_size * 2 * bilstm, self.hidden_size),
+            self.attention = nn.Sequential(
+                nn.Conv2d(in_chans, out_chans, 3, padding=1),
                 nn.ReLU(),
-                nn.Linear(self.hidden_size, 1)
-            ])
+                nn.Conv2d(out_chans, out_chans, 3, padding=1))
 
-    def forward(self, decoder_hiddens, encoder_outputs):
-        batch_size = encoder_outputs.size()[0]
-        energies = torch.zeros(batch_size, self.pedestrian_num)
-        energies = energies.cuda() if self.use_cuda else energies
-        for i in range(self.pedestrian_num):
-            energies[:, i] = self._score(decoder_hiddens[:, i, :], encoder_outputs[:, i, :])
-        return F.softmax(energies, dim=-1)
+    def forward(self, x1, x2):
+        energy = self._score(x1, x2)
+        return F.softmax(energy)
 
-    def _score(self, hidden, encoder_output):
+    def _score(self, x1, x2):
         """Calculate the relevance of a particular encoder output in respect to the decoder hidden."""
 
         if self.method == 'dot':
-            energy = torch.bmm(hidden.unsqueeze(1), encoder_output.unsqueeze(2)).squeeze(-1)
+            energy = torch.bmm(x1.unsqueeze(1), x2.unsqueeze(2))
         elif self.method == 'general':
-            energy = self.attention(encoder_output)
-            energy = torch.bmm(hidden.unsqueeze(1), energy.unsqueeze(2)).squeeze(-1)
+            energy = self.attention(x2)
+            energy = torch.bmm(x1.unsqueeze(1), energy.unsqueeze(2))
         elif self.method == 'concat':
-            energy = self.attention[0](torch.cat((hidden, encoder_output), -1))
-            energy = self.attention[1](energy)
-            energy = self.attention[2](energy)
-        return energy.squeeze(-1)
+            energy = self.attention(torch.cat((x1, x2), dim=1))
+        return energy
 
 
 class DepthUpSample(nn.Module):
-    def __init__(self, in_channel, out_channel):
+    def __init__(self, in_channel, out_channel, low_size):
         super(DepthUpSample, self).__init__()
+        self.low_conv = nn.Sequential(
+            nn.Conv2d(low_size+256, low_size, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(low_size, low_size, 3, padding=1))
         self.conv = nn.Sequential(nn.Conv2d(in_channel, out_channel, 3, padding=1),
                                   nn.LeakyReLU(0.2),
                                   nn.Conv2d(out_channel, out_channel, 3, padding=1),
                                   nn.LeakyReLU(0.2))
 
-    def forward(self, input):
-        up_x = F.interpolate(input[0], size=input[1].size()[2:], mode='bilinear', align_corners=True)
-        output = self.conv(torch.cat((up_x, input[1]), dim=1))
+    def forward(self, x, low_feature, seg_low):
+        up_x = F.interpolate(x, size=low_feature.size()[2:], mode='bilinear', align_corners=True)
+        low_feature = self.low_conv(torch.cat((low_feature, seg_low), dim=1))
+        output = self.conv(torch.cat((up_x, low_feature), dim=1))
         return output
 
 
@@ -97,44 +92,46 @@ class SegModel(nn.Module):
 
     def forward(self, x, low_level_features):
         # semantic segmentation
-        seg_x = self.seg_up1(x, low_level_features[0])
-        seg_x = self.seg_up2(seg_x, low_level_features[1])
-        seg_x = self.seg_up3(seg_x, low_level_features[2])
-        seg_out = self.seg_last(seg_x)
-        return seg_out
+        seg_1 = self.seg_up1(x, low_level_features[0])
+        seg_2 = self.seg_up2(seg_1, low_level_features[1])
+        seg_3 = self.seg_up3(seg_2, low_level_features[2])
+        seg_out = self.seg_last(seg_3)
+        return seg_out, (seg_1, seg_2, seg_3)
 
 
 class DepthModel(nn.Module):
-    def __init__(self, chan):
+    def __init__(self, chan, low_feature_sizes):
         super(DepthModel, self).__init__()
         # depth estimation
         self.depth_conv1 = nn.Conv2d(chan, chan, kernel_size=1, stride=1, padding=1)
-        self.depth_up1 = DepthUpSample(chan // 1 + 512, chan // 2)
-        self.depth_up2 = DepthUpSample(chan // 2 + 256, chan // 4)
-        self.depth_up3 = DepthUpSample(chan // 4 + 64, chan // 8)
+        self.depth_up1 = DepthUpSample(chan // 1 + low_feature_sizes[0], chan // 2, low_feature_sizes[0])
+        self.depth_up2 = DepthUpSample(chan // 2 + low_feature_sizes[1], chan // 4, low_feature_sizes[1])
+        self.depth_up3 = DepthUpSample(chan // 4 + low_feature_sizes[2], chan // 8, low_feature_sizes[2])
         self.depth_conv2 = nn.Conv2d(chan // 8, 1, kernel_size=3, stride=1, padding=1)
         self.depth_relu2 = nn.LeakyReLU(0.2)
 
-    def forward(self, x, low_level_features):
+    def forward(self, x, low_level_features, seg_low):
         # depth estimation
         depth_x = self.depth_conv1(x)
-        depth_x = self.depth_up1([depth_x, low_level_features[0]])
-        depth_x = self.depth_up2([depth_x, low_level_features[1]])
-        depth_x = self.depth_up3([depth_x, low_level_features[2]])
-        depth_out = self.depth_relu2(self.depth_conv2(depth_x))
-        return depth_out
+        depth_1 = self.depth_up1(depth_x, low_level_features[0], seg_low[0])
+        depth_2 = self.depth_up2(depth_1, low_level_features[1], seg_low[1])
+        depth_3 = self.depth_up3(depth_2, low_level_features[2], seg_low[2])
+        depth_out = self.depth_relu2(self.depth_conv2(depth_3))
+        return depth_out, (depth_1, depth_2, depth_3)
 
 
 class OdometryModel(nn.Module):
     def __init__(self, chan, img_size):
         super(OdometryModel, self).__init__()
-
+        self.low_pool = nn.MaxPool2d(2, 2)
+        self.attention1 = Attention(512, 256, method='concat')
+        self.attention2 = Attention(384, 128, method='concat')
         # vision odometry
         vo_chan = 2
         self.vo_conv = nn.Conv2d(chan, vo_chan, kernel_size=1, stride=1)
         in_size = vo_chan * img_size[0] * img_size[1] / args.output_scale / args.output_scale
         # print("in_size is {}".format(in_size))
-        self.rnn = nn.LSTM(
+        self.lstm = nn.LSTM(
                     input_size=int(in_size),
                     hidden_size=args.rnn_hidden_size,
                     num_layers=2,
@@ -143,11 +140,15 @@ class OdometryModel(nn.Module):
         self.rnn_drop_out = nn.Dropout(args.rnn_dropout_out)
         self.linear = nn.Linear(in_features=args.rnn_hidden_size, out_features=6)
 
-    def forward(self, x, low_level_features, batch_size, seq_len):
+    def forward(self, x, low_feature1, low_feature2, batch_size, seq_len):
+        low_feature1 = self.low_pool(low_feature1)
+        low_feature2 = self.low_pool(low_feature2)
+        low_feature1 = torch.mul(low_feature1, self.attention1(x, low_feature1))
+        low_feature2 = torch.mul(low_feature2, self.attention2(x, low_feature2))
         # vision odometry
-        vo_x = self.vo_conv(x)
+        vo_x = self.vo_conv(torch.cat((low_feature1, low_feature2), dim=1))
         vo_x = vo_x.view(batch_size, seq_len, -1)
-        vo_out, hc = self.rnn(vo_x)
+        vo_out, hc = self.lstm(vo_x)
         vo_out = self.rnn_drop_out(vo_out)
         vo_out = self.linear(vo_out)
         return vo_out
@@ -159,30 +160,30 @@ class Decoder(nn.Module):
         self.args = args
         img_size = args.crop_size
         if args.backbone == 'resnext':
-            self.low_feature_size = [512, 256, 64]
+            self.low_feature_sizes = [512, 256, 64]
             self.num_channels = 256
         else:
             raise NotImplementedError
         self.logsigma = nn.Parameter(torch.FloatTensor([-0.5, -0.5, -0.5]))
 
-        self.seg_decoder = SegModel(self.low_feature_size)
+        self.seg_decoder = SegModel(self.low_feature_sizes)
 
         chan = int(self.num_channels)
-        self.depth_decoder = DepthModel(chan)
+        self.depth_decoder = DepthModel(chan, self.low_feature_sizes)
 
-        self.vo_decoder = OdometryModel(chan, img_size)
+        self.vo_decoder = OdometryModel(int(chan*1.5), img_size)
 
         initial_weight(self.modules())
 
     def forward(self, x, low_level_features, seq_len):
         # semantic segmentation
-        seg_out = self.seg_decoder(x, low_level_features)
+        seg_out, seg_low = self.seg_decoder(x, low_level_features)
 
         # depth estimation
-        depth_out = self.depth_decoder(x, low_level_features)
+        depth_out, depth_low = self.depth_decoder(x, low_level_features, seg_low)
 
         # vision odometry
-        vo_out = self.vo_decoder(x, low_level_features, self.args.batch_size, seq_len)
+        vo_out = self.vo_decoder(x, seg_low[0], depth_low[0], self.args.batch_size, seq_len)
 
         return [depth_out, seg_out, vo_out], self.logsigma
 
